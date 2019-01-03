108f6d8fb47d2dddd9c3491a39edb8edb52ce18a
Jeff Lee <jeffomatic@gmail.com>
2019-01-02 21:38:37 -0800

    switch to using PyMuPDF lib to extract text

--- tsukemen-alert.py
+++ tsukemen-alert.py
@@ -1,7 +1,7 @@
 import calendar
 import datetime
+import fitz as pymupdf  # PyMuPDF
 import json
-import PyPDF2
 import re
 import os
 import slackclient
@@ -39,8 +39,13 @@ def load_config():
     return config
 
 
-# calendar.month_name is 1-indexed
-months = [calendar.month_name[i].lower() for i in range(0, 13)]
+# Ramen Shop's menus render month names with arbitrary spaces
+month_patterns = [
+    'j *a *n *u *a *r *y', 'f *e *b *r *u *a *r *y', 'm *a *r *c *h',
+    'a *p *r *i *l', 'm *a *y', 'j *u *n *e', 'j *u *l *y', 'a *u *g *u *s *t',
+    's *e *p *t *e *m *b *e *r', 'o *c *t *o *b *e *r',
+    'n *o *v *e *m *b *e *r', 'd *e *c *e *m *b *e *r'
+]
 
 
 def guess_year(month):
@@ -56,20 +61,26 @@ def guess_year(month):
     return now.year
 
 
-def get_date(lines):
-    n = None
-    month = None
-    for i, line in enumerate(lines):
-        if line in months:
-            n = i
-            month = months.index(line)
-            break
+def parse_date(lines):
+    def get_month_and_line():
+        for i, line in enumerate(lines):
+            for m, pat in enumerate(month_patterns):
+                if re.fullmatch(pat, line):
+                    return (m + 1, i)  # Return the 1-indexed month
 
-    # Date line follows the month
-    if n is None or n >= len(lines) - 1:
+        return (None, None)
+
+    month, monthline = get_month_and_line()
+    if monthline is None:
         return None
 
-    match = re.search(r"(\d+)", lines[n + 1])
+    # Date line follows the month after a delimeter line
+    dateline = monthline + 2
+    if dateline >= len(lines):
+        return None
+
+    # Ramen Shop has a delimeter between the month and date
+    match = re.search(r"(\d+)", lines[dateline])
     if match is None:
         return None
 
@@ -85,25 +96,30 @@ def has_substr(lines, substr):
     return False
 
 
-def perform_fetch():
-    with fetch('https://s3.amazonaws.com/ramenshop/dinner.pdf') as path:
-        fr = PyPDF2.PdfFileReader(open(path, 'rb'))
-        lines = []
-        for i in range(0, fr.getNumPages() - 1):
-            p = fr.getPage(i)
-            s = p.extractText()
+def perform_fetch(url):
+    lines = []
+    with fetch(url) as path:
+        pdf = pymupdf.open(path)
+        for i in range(pdf.pageCount):
+            s = pdf.getPageText(i)
             for line in s.split('\n'):
                 line = line.strip().lower()
                 if len(line) > 0:
                     lines.append(line)
+        pdf.close()
 
-        msg = 'No tsukemen...'
-        if has_substr(lines, 'tsukemen'):
-            msg = 'Has tsukemen!'
+    msg = 'No tsukemen...'
+    if has_substr(lines, 'tsukemen'):
+        msg = 'Has tsukemen!'
 
-        date = get_date(lines)
+    date = parse_date(lines)
     print("%s: %s" % (date, msg))
 
+
+menu_url = 'https://s3.amazonaws.com/ramenshop/dinner.pdf'
+url_no_tsukemen = 'https://www.dropbox.com/s/a01gksgh7nle94g/sample-no-tsukemen.pdf?dl=1'
+url_with_tsukemen = 'https://www.dropbox.com/s/c85wln91upjn8ma/sample-with-tsukemen.pdf?dl=1'
+perform_fetch(url_with_tsukemen)
 
 config = load_config()
 sc = slackclient.SlackClient(config['SLACK_API_TOKEN'])
